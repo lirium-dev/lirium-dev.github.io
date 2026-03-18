a6db1d10522230edd1841c2f11572f9a801b2f86
lirium <81167211+lirium-dev@users.noreply.github.com>
2026-03-18 13:49:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,133 +8,63 @@ ready(main);
 
 function main() {
 
-    var oben
-    var mid
-    var but1_down
-    var but2_down
-    var but3_down
 
-    var count = 0
+const skills = ["Frontend Developer", "Backend Developer", "Graphic Designer", "Video Editor", "Perfectionist"]
 
-    oben = document.getElementById("oben")
-    mid = document.getElementById("mid")
-    but1_down = document.getElementById("but1_down")
-    but2_down = document.getElementById("but2_down")
-    but3_down = document.getElementById("but3_down")
+let varT = document.getElementById("varT")
 
-    document.getElementById("but1_down").onclick = function() {
- 
+
+var textIndex = 0; 
+var i = 0; 
+var speed = 120;  
+
+var deleting = false; 
+
+function typeWriter() {
+  var currentText = skills[textIndex]; 
+
+  if (!deleting && i < currentText.length) {
   
-        if(document.getElementById("but1_down").classList.contains("Step1")){
- 
-            document.getElementById("but1_down").classList.add("Step2")
-            document.getElementById("but2_down").classList.add("Step2")
-            Step2()
-          
-            if(document.getElementById("but1_down").innerHTML == "YESS 🥰"){
-                document.getElementById("but1_down").classList.add("Step3")
-            document.getElementById("but2_down").classList.add("Step3")
-       
-            }
+    varT.innerHTML += currentText.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  } else if (deleting && i >= 0) {
+    
+    varT.innerHTML = "I'm a " + currentText.substring(0, i);
+    i--;
+    setTimeout(typeWriter, speed);
+  } else {
 
-      
-           
-
-        }else{
-            document.getElementById("mid_pic").src = "/img/blue-blush-emoji.webp"
-             document.getElementById("top").innerHTML = "Kinda nervous, ngl 😧"
-            document.getElementById("but1_down").innerHTML="YEA SAME :3"
-              document.getElementById("but2_down").innerHTML="WHAT DO YOU WANT?! 🙄"
-          
-          
-            }
-
-
-        document.getElementById("but1_down").classList.add("Step1")
-
-
-
-        if(document.getElementById("but2_down").classList.contains("invisible")){
-            count = 1
-            Step3();
-            document.getElementById("but1_down").classList.add("FINAL")
+    if (!deleting) {
+     
+      deleting = true;
+      setTimeout(typeWriter, speed);
+    } else {
    
-        }
+      deleting = false;
+      textIndex = (textIndex + 1) % skills.length;  
+      i = 0;  
+      setTimeout(typeWriter, speed);
+    }
+  }
+}
 
-        if(document.getElementById("but1_down").classList.contains("FINAL")){
-            setTimeout(() => {
-                StepFinal();
-            }, 5000);
-              
-          
-         
-        }
+typeWriter();  
+
+    
+
+
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
   
-        if(document.getElementById("but1_down").classList.contains("Step3")) {
-          
-            document.getElementById("but2_down").classList.add("invisible")
-   
-        }
-
-    
-
-      };
-
-      document.getElementById("but2_down").onclick = function() {
-        if(!document.getElementById("but1_down").classList.contains("Step1")){
-        document.getElementById("mid_pic").src = "/img/sad-cat-sad.gif"
-        document.getElementById("top").innerHTML= "Oh Damn 💔"
-        setTimeout(() => {
-            location.reload()
-        }, 8000);
-    }else{
-
-        document.getElementById("but1_down").classList.add("Step2")
-        document.getElementById("but2_down").classList.add("Step2")
-        Step2();
-
-
-        document.getElementById("but1_down").classList.add("Step3")
-        document.getElementById("but2_down").classList.add("Step3")
-        alert("EXCUSE ME?? NUH UH THATS NOT THE RIGHT ANSWER")
-    
-
-    }
-
-
-
-    };
-   
-}
-
-
-function Step2() {
-    
-    document.getElementById("mid_pic").src = "/img/ques.png"
-    document.getElementById("top").innerHTML = "next Week Sonntag haben wir unser zweites DATE?"
-   document.getElementById("but1_down").innerHTML="YESS 🥰"
-     document.getElementById("but2_down").innerHTML="unfortunately 🙄"
-
-}
-
-
-function Step3()  { 
-
-
-    document.getElementById("mid_pic").src = "/img/flush.png"
-    document.getElementById("top").innerHTML = "BIST DU FINE MIT SPAZIEREN GEHEN UND DANN VLLT EIN CAFE?"
-     document.getElementById("but1_down").innerHTML="YES SURE :3"
-    document.getElementById("mid_2").classList.remove("invisible")
-     document.getElementById("mid_2").innerHTML = "If u press YES (hopfully) dann u need to wait 5 Seconds 😧"
-
-}
-
-function StepFinal(){
-
-     document.getElementById("but2_down").classList.add("invisible")
-        document.getElementById("mid_pic").src = "/img/signthis.png"
-    document.getElementById("top").innerHTML = "SIGN THIS THAN"
-    document.getElementById("but1_down").innerHTML ="Sign"
-    // document.getElementById("mid_2").classList.add("invisible")
-      document.getElementById("mid_2").innerHTML = "ich hoffe du hast auf den button gedrückt WENN JA DANN SOLLST DU WISSEN DAS ICH DICH LIEB HAB DU TOLLER MENSCH MEINE :3"
-    }
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+  }
